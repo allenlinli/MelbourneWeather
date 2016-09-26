da3f7e5a4bd1035355001b90b2d73ef709678015
Li Lin <allenlinli@gmail.com>
2016-09-27 07:14:12 +1000

    Initial Commit

---
+++ WeatherForecast/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeatherForecast
+//
+//  Created by allenlinli on 9/27/16.
+//  Copyright © 2016 Raccoonism. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
